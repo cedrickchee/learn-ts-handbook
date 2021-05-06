099ad560cc89f9c2f018c833c3af20cdac07409a
Cedric Chee <cedric@invictusbyte.com>
2021-05-06 21:58:21 +0800

    expose modules

---
+++ src/everyday-types/index.ts
@@ -0,0 +1,16 @@
+import { pc } from './type-aliases';
+import { printId, printId2, welcomePeople, getFirstThree } from './union-types';
+import { greet, getFavoriteNumber } from './functions';
+import { printCoord, printName } from './object-types';
+
+export {
+  pc,
+  printId,
+  printId2,
+  welcomePeople,
+  getFirstThree,
+  greet,
+  getFavoriteNumber,
+  printCoord,
+  printName,
+};
