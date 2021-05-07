3a564703c5115e4ab9748cad1ab0f76ece5d9d62
Cedric Chee <cedric@invictusbyte.com>
2021-05-07 21:06:13 +0800

    narrowing: truthiness narrowing

--- src/narrowing/index.ts
+++ src/narrowing/index.ts
@@ -1,4 +1,5 @@
 import { padLeft } from './narrowing';
 import { printAll } from './type-guards';
+import { anotherPrintAll, multiplyAll } from './truthiness-narrowing';
 
-export { padLeft, printAll };
+export { padLeft, printAll, anotherPrintAll, multiplyAll };
---
+++ src/narrowing/truthiness-narrowing.ts
@@ -0,0 +1,47 @@
+/**
+ * Truthiness narrowing
+ */
+
+// guarding against values like null or undefined.
+
+function printAll(strs: string | string[] | null) {
+  if (strs && typeof strs === 'object') {
+    for (const s of strs) {
+      console.log(s);
+    }
+  } else if (typeof strs === 'string') {
+    console.log(strs);
+  }
+}
+
+function anotherPrintAll(strs: string | string[] | null) {
+  // !!!!!!!!!!!!!!!!
+  //  DON'T DO THIS!
+  //   KEEP READING
+  // !!!!!!!!!!!!!!!!
+  if (strs) {
+    if (typeof strs === 'object') {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === 'string') {
+      console.log(strs);
+    }
+  }
+}
+
+// Boolean negations with `!`.
+function multiplyAll(
+  values: number[] | undefined,
+  factor: number
+): number[] | undefined {
+  if (!values) {
+    return values;
+  } else {
+    return values.map((x) => x * factor);
+  }
+}
+
+console.log(multiplyAll([1, 2, 3], 2));
+
+export { printAll, anotherPrintAll, multiplyAll };
