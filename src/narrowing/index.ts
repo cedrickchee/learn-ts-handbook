617d1f0b2b9666f12a8a0f6906cca721ae0d3b1a
Cedric Chee <cedric@invictusbyte.com>
2021-05-09 15:43:26 +0800

    narrowing: `instanceof` narrowing

--- src/narrowing/index.ts
+++ src/narrowing/index.ts
@@ -3,5 +3,14 @@ import { printAll } from './type-guards';
 import { anotherPrintAll, multiplyAll } from './truthiness-narrowing';
 import { example } from './equality-narrowing';
 import { move } from './in-operator-narrowing';
+import { logValue } from './instanceof-narrowing';
 
-export { padLeft, printAll, anotherPrintAll, multiplyAll, example, move };
+export {
+  padLeft,
+  printAll,
+  anotherPrintAll,
+  multiplyAll,
+  example,
+  move,
+  logValue,
+};
---
+++ src/narrowing/instanceof-narrowing.ts
@@ -0,0 +1,16 @@
+/**
+ * `instanceof` narrowing
+ */
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+logValue(new Date());
+logValue("Something");
+
+export { logValue };
