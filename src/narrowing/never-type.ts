ffaa55e6107d37b5167bff85f1e0424c4b317225
Cedric Chee <cedric@invictusbyte.com>
2021-05-10 02:03:21 +0800

    narrowing: the never type

---
+++ src/narrowing/never-type.ts
@@ -0,0 +1,10 @@
+/**
+ * The `never` type
+ */
+
+// When narrowing, you can reduce the options of a union to a point where you
+// have removed all possibilities and have nothing left.
+// In those cases, TypeScript will use a never type to represent a
+// state which shouldn’t exist.
+
+export {};
