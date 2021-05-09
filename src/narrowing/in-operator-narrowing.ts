f05cbc7523e218fef95cd279b4d0381a0fa6474c
Cedric Chee <cedric@invictusbyte.com>
2021-05-09 15:36:39 +0800

    narrowing: `in` operator narrowing

---
+++ src/narrowing/in-operator-narrowing.ts
@@ -0,0 +1,17 @@
+/**
+ * The `in` operator narrowing
+ */
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+// type Human = { swim?: () => void, fly?: () => void };
+
+function move(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    return animal.swim();
+  }
+
+  return animal.fly();
+}
+
+export { move };
--- src/narrowing/index.ts
+++ src/narrowing/index.ts
@@ -2,5 +2,6 @@ import { padLeft } from './narrowing';
 import { printAll } from './type-guards';
 import { anotherPrintAll, multiplyAll } from './truthiness-narrowing';
 import { example } from './equality-narrowing';
+import { move } from './in-operator-narrowing';
 
-export { padLeft, printAll, anotherPrintAll, multiplyAll, example };
+export { padLeft, printAll, anotherPrintAll, multiplyAll, example, move };
